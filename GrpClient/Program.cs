e13b9298a2859a8e9996fda4a0783036a3226066
Aruna Meiyeppen <aruna.meiyeppen@gmail.com>
2021-05-09 11:52:59 -0700

    Add project files.

---
+++ GrpClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GrpClient;
+using GrpcServer.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            
+            //var input = new HelloRequest { Name = "Aruna" };
+            //var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(input);
+            //Console.WriteLine(reply.Message);
+
+            var customerClient = new Customer.CustomerClient(channel);
+            var customerLookupModel = new CustomerLookupModel { UserId = 1 };
+            var customerResponse = await customerClient.GetCustomerInfoAsync(customerLookupModel);
+
+            Console.WriteLine(customerResponse.FirstName);
+            Console.WriteLine(customerResponse.LastName);
+            Console.WriteLine(customerResponse.EmailAddress);
+            Console.WriteLine(customerResponse.IsAtive);
+
+            Console.WriteLine();
+            Console.WriteLine("New Customers:");
+            Console.WriteLine();
+
+            using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var currentCust = call.ResponseStream.Current;
+                    Console.WriteLine($"{currentCust.FirstName} {currentCust.LastName} {currentCust.EmailAddress} {currentCust.IsAtive}");
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ GrpcServer/Services/CustomerService.cs
@@ -0,0 +1,65 @@
+﻿using Grpc.Core;
+using GrpcServer.Protos;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel output = new CustomerModel
+            {
+                EmailAddress = "aruna@mail.com",
+                FirstName = "roonz",
+                LastName = "moneypenny",
+                IsAtive = true
+            };
+
+            return Task.FromResult(output);
+        }
+
+        public override async Task GetNewCustomers(NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> newCustomers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    EmailAddress = "new1@mail.com",
+                    FirstName = "new1",
+                    LastName = "new1 moneypenny",
+                    IsAtive = true
+                },
+                new CustomerModel
+                {
+                    EmailAddress = "new2@mail.com",
+                    FirstName = "new2",
+                    LastName = "new2 moneypenny",
+                    IsAtive = true
+                },
+                new CustomerModel
+                {
+                    EmailAddress = "new3@mail.com",
+                    FirstName = "new3",
+                    LastName = "new3 moneypenny",
+                    IsAtive = true
+                },
+            };
+
+            foreach (var cust in newCustomers)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(cust);
+            }
+        }
+    }
+}
